d6d9d239701a075e9257a42540cedf8e66d27ae2
JJ Merelo <jjmerelo@gmail.com>
2015-09-20 10:50:17 +0200

    Segundo que no funciona...

--- fp-scala/1.scala
+++ fp-scala/1.scala
@@ -10,7 +10,21 @@ object ValorAbsoluto {
     var msg= "El valor absoluto de %d es %d."
     msg.format(x, abs(x))
   }
-  
+
+  private def formatFact(x:Int) = {
+    var msg= "El factorial de %d es %d."
+    msg.format(x, factorial(x))
+  }
+
+  def factorial(n: Int): Int = {
+    def go( n: Int, acc: Int): Int = 
+      if (n <= 0) acc
+      else go( n-1, n*acc )
+
+    go( n, 1 )
+  }
+
   def main(args: Array[String]): Unit =
-    println(formatAbs(-42))
+    println(formatAbs(-42)) // este no sale
+    println(formatFact( 8 ))
 }
