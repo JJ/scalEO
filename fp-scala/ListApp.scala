72d6e4ed0380e0c519b795f4862e83b1e3d0d05d
JJ Merelo <jjmerelo@gmail.com>
2015-09-24 07:36:39 +0200

    Checked exercise 3.3

--- fp-scala/ListApp.scala
+++ fp-scala/ListApp.scala
@@ -3,23 +3,28 @@
 import fpinscala.datastructures.Cons
 import fpinscala.datastructures.List
 import fpinscala.datastructures.Nil
+import fpinscala.datastructures
 
 object ListApp extends App {
-    val example = Cons(1, Cons(2, Cons(3, Nil)))
-    val example2 = List(1,2,3)
-    val total = List.sum(example)
-    println("Total = " + total)
-    val example3 = List(1.0, 2.0, 3.0)
-    val product = List.product(example3)
-    println("Product = " + product)
-    
-    val x = List(1,2,3,4,5) match {
-      case Cons(x, Cons(2, Cons(4, _))) => x
-      case Nil => 42
-      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-      case Cons(h, t) => h + List.sum(t)
-      case _ => 101
-    }
-    println("x = " + x)
+  val example = Cons(1, Cons(2, Cons(3, Nil)))
+  val example2 = List(1,2,3)
+  val total = List.sum(example)
+  println("Total = " + total)
+  val example3 = List(1.0, 2.0, 3.0)
+  val product = List.product(example3)
+  println("Product = " + product)
+  
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + List.sum(t)
+    case _ => 101
+  }
+
+  println("x = " + x)
+
+  val ex34 = List.tail(List( 4,3,2, 1 ))
+  println( "ex34 = " + ex34 )
 
 }
--- fp-scala/ex3.4.scala
+++ fp-scala/ex3.4.scala
@@ -23,14 +23,6 @@ object List { // `List` companion object. Contains functions for creating and wo
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
-  }
-
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
