2ddf397c0ed1fe9e2d4b318c08ea8c60edadabe8
JJ Merelo <jjmerelo@gmail.com>
2015-10-18 09:05:01 +0200

    Alternative that does not need Fitness trait

--- org/geneura/algorithm/evolutionary/EO.scala
+++ org/geneura/algorithm/evolutionary/EO.scala
@@ -1,5 +1,7 @@
 package org.geneura.algorithm.evolutionary.EO
 
-import org.geneura.algorithm.evolutionary.Fitness._
 
-trait EO[A,B] extends Fitness[A,B]
+trait EO[A,B] {
+  def fitness:B
+  def dataObject:A
+}
