974d1947b297e9e1b24be407f6c0f82ba7d13545
JJ Merelo <jjmerelo@gmail.com>
2015-10-18 08:58:39 +0200

    Añade objeto evolucionable

---
+++ org/geneura/algorithm/evolutionary/EO.scala
@@ -0,0 +1,5 @@
+package org.geneura.algorithm.evolutionary.EO
+
+import org.geneura.algorithm.evolutionary.Fitness._
+
+trait EO[A,B] extends Fitness[A,B]
--- org/geneura/algorithm/evolutionary/Fitness.scala
+++ org/geneura/algorithm/evolutionary/Fitness.scala
@@ -1,5 +1,6 @@
 package org.geneura.algorithm.evolutionary.Fitness
 
+//Fitness acting on A objects and returning B
 trait Fitness[A,B] {
   def Fitness( ev: A):B
 }
