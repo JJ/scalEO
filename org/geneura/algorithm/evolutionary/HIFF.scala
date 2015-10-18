a2731acb559c1f86c81d618839dc5e68e4ce9c0b
JJ Merelo <jjmerelo@gmail.com>
2015-10-18 11:40:14 +0200

    New version with Strings

--- org/geneura/algorithm/evolutionary/HIFF.scala
+++ org/geneura/algorithm/evolutionary/HIFF.scala
@@ -10,11 +10,11 @@ object HIFF extends Function1[Vector[Boolean],Double] with Fitness[Vector[Boolea
     case (_,_) => '-'
   }
 
-  def T( ev: Vector[Char] ): Char = ev match {
-    case Vector('-')|Vector('0')|Vector('1') => ev.head
-    case Vector('0','0') => '0'
-    case Vector('1','1') => '1'
-    case Vector(_,'1')|Vector(_,'0')| Vector('0',_)| Vector('1',_) => '-'
+  def T( ev: String ): Char = ev match {
+    case "-"|"0"|"1" => ev.head
+    case "00" => '0'
+    case "11" => '1'
+    case "01"|"10"|"1-"|"--"|"-1"|"0-"|"-0" => '-'
     case _ => t(T(ev.take(ev.length/2)),T(ev.drop(ev.length/2)))
   }
 
@@ -23,16 +23,16 @@ object HIFF extends Function1[Vector[Boolean],Double] with Fitness[Vector[Boolea
     case _ => 0
   }
 
-  def charFitness( ev: Vector[Char]):Double = ev match {
-    case Vector('0')|Vector('1') => 1
-    case Vector('0','0')|Vector('1','1') => 4
-    case Vector('0','1')|Vector('1','0') => 2
+  def charFitness( ev: String):Double = ev match {
+    case "0"|"1" => 1
+    case "00"|"11" => 4
+    case "01"|"10" => 2
     case _ =>
       ev.length * f(T(ev)) + charFitness(ev.take(ev.length/2)) + charFitness(ev.drop(ev.length/2))
   }
 
   def Fitness( ev: Vector[Boolean]):Double = {
-    val char_ev = ev.map(  (a: Boolean) => if (a == true) '1' else '0' ) 
+    val char_ev = ev.mkString.replace("true","1").replace("false","0")
     charFitness(char_ev)
   }
     
