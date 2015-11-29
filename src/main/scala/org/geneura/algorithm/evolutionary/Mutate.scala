69ccede751e552fd86b5ee69fa4a85d0df72b3b2
JJ Merelo <jjmerelo@gmail.com>
2015-11-29 19:55:47 +0100

    Modified mutation thanks to @ppgllrd

--- src/main/scala/org/geneura/algorithm/evolutionary/Mutate.scala
+++ src/main/scala/org/geneura/algorithm/evolutionary/Mutate.scala
@@ -1,11 +1,12 @@
 package org.geneura.algorithm.evolutionary.Mutate
 
 object Mutate {
-  def apply( eo : IndexedSeq[Boolean] ): IndexedSeq[Boolean] = {
-  val point = (eo.length * scala.util.Random.nextDouble).toInt
-  eo.take( point ) ++ eo.slice( point,point+1 ).map(!_) ++ eo.drop( point +1 )
-}
 
+  def apply( eo : IndexedSeq[Boolean] ): IndexedSeq[Boolean] = {
+    val point = scala.util.Random.nextInt( eo.length )
+    eo.updated(point, !eo(point))
+  }
+  
   def apply( eo : String ): String = {
     val point = (eo.length * scala.util.Random.nextDouble).toInt
     eo.take( point ) ++ eo.slice( point,point+1 ).map( (a:Char) => if ( a == '0' ) '1' else '0') ++ eo.drop( point +1 )
